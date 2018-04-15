04caa22db95f370d433b614d0e2ec2264b79497e
UrszulaSlusarz <32739935+UrszulaSlusarz@users.noreply.github.com>
2018-04-15 19:44:25 +0200

    Add files via upload

---
+++ Calculator_Swing_GUI/Runner.java
@@ -0,0 +1,12 @@
+package com.slusarzz.gui_swing;
+
+import com.slusarzz.gui_swing.ui.controller.MainFrameController;
+
+
+public class Runner {
+
+    public static void main( String[] args ) {
+
+        MainFrameController mainFrameController=new MainFrameController();
+    }
+}
